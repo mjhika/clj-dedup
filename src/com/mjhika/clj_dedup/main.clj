1da921b87c89e9b6c2d02b840dff5a9668362784
mjhika <1zzt6ovh@mojica.anonaddy.com>
2025-10-06 00:47:27 -0400

    feat: help and csv export
    exports results to csv upon reception of a directory
    prints help dialogue if argument supplied is not a directory

--- src/com/mjhika/clj_dedup/main.clj
+++ src/com/mjhika/clj_dedup/main.clj
@@ -1,17 +1,26 @@
 (ns com.mjhika.clj-dedup.main
   (:require
+   [charred.api :as charred]
    [clojure.java.io :as io]
    [clojure.pprint :as pp])
   (:import
+   (java.io File)
+   (java.nio.file Files)
    (java.security MessageDigest)
-   (java.nio.file Files))
+   (java.text SimpleDateFormat)
+   (java.util Date))
   (:gen-class))
 
 (set! *warn-on-reflection* true)
 
+(defn file? [^File s]
+  (.isFile s))
+
+(defn directory? [^File s]
+  (.isDirectory s))
+
 (defn enum-files [dir]
-  (filter #(.isFile ^java.io.File %)
-          (file-seq (io/file dir))))
+  (filter file? (file-seq (io/file dir))))
 
 (defn file-details [file]
   [(.length (io/file file)) (str file)])
@@ -33,9 +42,38 @@
 (defn hash-file [file]
   [(sha512sum (file->bytes file)) file])
 
+(defn inspect [d]
+  (pp/pprint d)
+  d)
+
+(defn entry->csv-data [[k s]]
+  (mapv #(vector % k) s))
+
+(defn timestamp []
+  (let [df (new SimpleDateFormat "yyyy-MM-dd'T'HHmmss")]
+    (.format df (new Date))))
+
+(defn help []
+  (println "clj-dedup - File duplicate discovery and resolution tool")
+  (newline)
+  (println "SYNOPSIS")
+  (println "\tclj-dedup directory")
+  (newline)
+  (println "DESCRIPTION")
+  (println "\tclj-dedup is a utility for finding duplicate files.")
+  (println "\tThe only valid usage of this utility is to supply a directory."))
+
+(defn parse-arg [arg]
+  (let [f (io/file arg)]
+    (when (directory? f) f)))
+
 (defn -main [& args]
-  (let [atm (atom {})]
-    (->> (enum-files (or (first args) "."))
+  (let [valid-dir (parse-arg (or (first args) "."))
+        atm (atom {})]
+    (when-not valid-dir
+      (help)
+      (System/exit 1))
+    (->> (enum-files valid-dir)
          (map file-details)
          (#(doseq [tup %]
              (swap! atm assoc-entry tup))))
@@ -45,7 +83,16 @@
            (map hash-file)
            (#(doseq [tup %]
                (swap! final assoc-entry tup))))
-      (pp/pprint (into {} (filter v>1? @final))))))
+      (let [csv-data (->> (into {} (filter v>1? @final))
+                          inspect
+                          (mapv entry->csv-data))
+            filename (format "./%s_dupes.csv" (timestamp))]
+        (with-open [w (io/writer filename)]
+          (charred/write-csv
+           w
+           (apply concat [["filepath" "sha512-checksum"]] csv-data)))
+        (newline)
+        (println "See" filename "for a CSV export.")))))
 
 (comment
   (sha512sum (file->bytes "deps.edn"))
